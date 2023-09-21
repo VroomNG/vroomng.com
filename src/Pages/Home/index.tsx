591d10eacf04473d4633164ad79456d044a303ed
xenxei46 <wisdomjohnikoi@gmail.com>
2023-09-21 08:10:51 -0700

    responsiveness

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -23,26 +23,26 @@ export default function index() {
     <div className="h-full w-full bg-white " >
        <header className="h-[120vh] bg-neutral">
             <div className="flex justify-center pt-[10%] pb-[5%] w-full h-full" >
-                <div className="container flex  w-[90%]">
-                    <img src={vector} className="z-[100] mt-[30%] ml-[40%] absolute"/>
-                    <div className="flex justify-center items-center w-[55%]">
+                <div className="container flex flex-wrap md:flex-nowrap lg:flex-nowrap xl:flex-nowrap w-[90%]">
+                    <img src={vector} className="z-[100] w-[40%] mt-[160%] md:w-[20%] lg:w-[20%] lg:mt-[30%] lg:ml-[40%] absolute"/>
+                    <div className="flex justify-center items-center mt-8 md:mt-0 lg:mt-0 xl:mt-0  w-[100%]  md:w-[55%] lg:w-[55%] xl:w-[55%]">
                         <div className=" h-[60%] w-[90%]">
                             <h1> Safe, Simple and Exciting Way to Ride. </h1>
                             <div className="w-[90%] py-4">
-                                <p>Be one one out first 100 drivers to get 3 months of commision
+                                <p>Be one out first 100 drivers to get 3 months of commision
                                     free earnings, when you join our Vroom Family!
                                 </p>
                             </div>
-                            <div className="flex w-[55%] justify-between py-4">
+                            <div className="flex w-[100%] md-w[55%] lg-w[55%] xl-w[55%] justify-between py-4 ml-2 ">
                                 {btn.map((item)=> {
                                     return(
                                         <div key={item.id}>
-                                            <button  className="bg-black flex w-40 p-1 text-white rounded-lg ">
-                                                <span className="w-[30%] p-1 flex items-center justify-center">
+                                            <button  className="bg-black flex w-36 md:w-40 text-white rounded-lg ">
+                                                <span className="w-[30%] p-2 flex items-center justify-center">
                                                     <img src={item.icon} width={"30px"}/>
                                                 </span>
                                                 <span className="w-[60] flex flex-col items-center justify-center">
-                                                    <p className="text-xs">{item.title}</p>
+                                                    <p className="text-[10px] md:text-xm lg:text-xm pt-1 md:pt-0 lg:pt-0 xl:pt-0 md:h-[20px] lg:h-[20px] xl:h-[20px]">{item.title}</p>
                                                     <p className="text-sm font-medium">{item.store}</p>
                                                 </span>    
                                             </button>
@@ -53,7 +53,7 @@ export default function index() {
                             </div>
                         </div>
                     </div>
-                    <div className=" w-[45%]">
+                    <div className=" w-[100%] mt-16 md:mt-0 lg:mt-0 xl:mt-0 md:w-[45%] lg:w-[45%] xl:w-[45%]">
                         <img src={drivergirl} className="h-[100%] w-[100%] " /> 
                     </div>
                 </div>
